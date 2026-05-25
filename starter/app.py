e9fea8424e92d96297faad93023a6d4316521135
Blake Mullinax <blakemullinax@charter.net>
2026-05-25 06:23:36 -0700

    Add logger

--- starter/app.py
+++ starter/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+import logging
+import sys
 
 app = Flask(__name__)
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     app.run(port=5000)
